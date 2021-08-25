0b8c2007a52e7efa0b039744bffac4448a45cfea
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-08-25 17:01:42 -0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -162,7 +162,7 @@ void sig_handler(int signum)
 */
 int main(int argc, char *argv[], char *envp[])
 {
-	char *command, *alias[1000], *filename, *new_envs[100];
+	char *command, *alias[1000], *filename, *new_envs[100] = NULL;
 	short getl_res, i, history_res = 1, file_res = 0;
 	int status = 0;
 
